671b0354a22c256378639f7d3cd49d2baded54a1
SZW568004887 <568004887@qq.com>
2018-09-19 13:53:22 +0800

    1

--- src/org/xmgreat/bean/LogBean.java
+++ src/org/xmgreat/bean/LogBean.java
@@ -5,7 +5,7 @@ public class LogBean //日志表
 	private Integer logId; //日志ID
 	private ManagerBean managerBean;//管理员bean
 	private String matter; //事项
-	private String operTime; //操作时间1
+	private String operTime; //操作时间
 
 	public LogBean() {
 		
