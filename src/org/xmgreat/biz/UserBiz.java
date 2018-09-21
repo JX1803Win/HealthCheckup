e6346e001b352554b4937b7fa76fa8c93c5686f0
sanotoko <550339643@qq.com>
2018-09-21 09:48:10 +0800

    9-21

--- src/org/xmgreat/biz/UserBiz.java
+++ src/org/xmgreat/biz/UserBiz.java
@@ -1,11 +1,11 @@
 package org.xmgreat.biz;
 
 import java.util.List;
-
 import org.xmgreat.bean.UserInfoBean;
 
 public interface UserBiz
 {
 	public List<UserInfoBean> checkUser(Long phone,String pwd);
+	public UserInfoBean checkPhone(Long phone);
 
 }
